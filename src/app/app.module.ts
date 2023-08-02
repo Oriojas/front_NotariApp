f5fb3fc00eaaa5c0a887551b657bf3b4782c6d14
oscar50 <oscar@whaleandjaguar.co>
2023-08-01 23:16:31 -0500

    feat: create new components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { WalletComponent } from './wallet/wallet.component';
+import { FormComponent } from './form/form.component';
+import { FileComponent } from './file/file.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WalletComponent,
+    FormComponent,
+    FileComponent
   ],
   imports: [
     BrowserModule,
