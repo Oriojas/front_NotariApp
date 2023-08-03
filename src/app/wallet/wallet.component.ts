9a80104865ddd6b86988250587e44453518e5ea0
oscar50 <oscar@whaleandjaguar.co>
2023-08-02 23:48:31 -0500

    feat: connection metamask

--- src/app/wallet/wallet.component.ts
+++ src/app/wallet/wallet.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { MetaMascaraFactory } from 'mmascara';
 
 @Component({
   selector: 'app-wallet',
@@ -7,4 +8,14 @@ import { Component } from '@angular/core';
 })
 export class WalletComponent {
 
+  async connect() {
+    const instance = MetaMascaraFactory.newInstance((window as any).detectEthereumProvider);
+    const result = await instance.connect();
+    const address = instance.address;
+
+    alert(`Connection result: ${result}\nAddress: ${address?.substring(0, 6)} ...`);
+
+  }
 }
+
+// https://youtu.be/Nuli86dRKlI
