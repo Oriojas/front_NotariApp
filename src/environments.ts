b1b85d3db1abe22eef987e3e76527cb09310d270
oscar50 <oscar@whaleandjaguar.co>
2023-08-07 22:09:43 -0500

    feat: update cache

--- src/environments.ts
+++
@@ -1,9 +0,0 @@
-export const environment = {
-    production: false,
-    WC_SECRET: 'sk_a37a88b5678d56c9c01b1c53a4cac105b78e86dc84895658',
-    WC_CLIENT_ID: 'app_313c05c45f23d36337513238c91b3d23',
-    AU_DOMAIN: 'dev-gm8ct5lal785w6j4.us.auth0.com',
-    AU_CLIENT_ID: 'ju8PnDCHf11pZTOWgPNsQ4lJ8MfkSMCA'
-
-  };
-  
