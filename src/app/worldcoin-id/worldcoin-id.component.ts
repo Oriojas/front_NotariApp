5d05fe95b57d1269be4850d1a46e0b0f4680cd20
oscar50 <oscar@whaleandjaguar.co>
2023-08-12 00:01:40 -0500

    test: user variable in prod

--- src/app/worldcoin-id/worldcoin-id.component.ts
+++ src/app/worldcoin-id/worldcoin-id.component.ts
@@ -9,5 +9,21 @@ import { AuthService } from '@auth0/auth0-angular';
   styleUrls: ['./worldcoin-id.component.scss']
 })
 export class WorldcoinIdComponent {
-  constructor(@Inject(DOCUMENT) public document: Document, public auth: AuthService) {}
+
+  constructor(@Inject(DOCUMENT) public document: Document,
+                                public auth: AuthService) {}
+
+  user_id_string: string | any;
+
+  user_login (event: Event): void {
+    const user_id = this.auth;
+
+    if (this.auth) {
+      this.user_id_string = user_id.user$;
+      console.log(this.user_id_string);
+    }
+  }
+
+  
+
 }
