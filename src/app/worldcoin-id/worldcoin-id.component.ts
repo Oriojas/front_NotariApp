9fb4f9125e39aa54dee6220f5a6fb860fc3533d5
oscar50 <oscar@whaleandjaguar.co>
2023-08-12 00:37:27 -0500

    feat: selctr substring id user

--- src/app/worldcoin-id/worldcoin-id.component.ts
+++ src/app/worldcoin-id/worldcoin-id.component.ts
@@ -11,19 +11,6 @@ import { AuthService } from '@auth0/auth0-angular';
 export class WorldcoinIdComponent {
 
   constructor(@Inject(DOCUMENT) public document: Document,
-                                public auth: AuthService) {}
-
-  user_id_string: string | any;
-
-  user_login (event: Event): void {
-    const user_id = this.auth;
-
-    if (this.auth) {
-      this.user_id_string = user_id.user$;
-      console.log(this.user_id_string);
-    }
-  }
-
-  
+                                public auth: AuthService) {}  
 
 }
